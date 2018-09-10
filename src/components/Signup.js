76339642ceccf1e18621ff78ef8e092f4a9fadb5
TezSmith <Montez.Smith1@gmail.com>
2018-09-10 19:30:29 -0400

    Added Favicon & Service Worker

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { InputField } from 'react-semantic-redux-form';
 import { Header, Button, Form, Grid, Segment} from 'semantic-ui-react'
-import carded from '../cardedv2.jpg'
+// import carded from '../cardedv2.jpg'
 
 const SignupForm = (props) => {
   const { handleSubmit, toggleSignup } = props
