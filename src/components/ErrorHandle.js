cac14ea13647f00aa36659ea6a9bda8419d7f34d
Tez Smith <montez.smith@flatironschool.com>
2019-05-22 17:51:34 -0400

    Added stateful HOC, dismissable error not working

--- src/components/ErrorHandle.js
+++ src/components/ErrorHandle.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Message } from 'semantic-ui-react'
 
-const errorHandling = WrappedComponent => ({ error, children }) => {
-    return (
-        <WrappedComponent>
-            {<div className="error-message">{error}</div>}
-            {children}
-        </WrappedComponent>
-    )
+// const errorHandling = WrappedComponent => ({ handleDismiss, error, children }) => {
+    
+//     return (
+//         <WrappedComponent>
+//             {error ? <Message onDismiss={() => handleDismiss} content={error}/> : null}
+//             {children}
+//         </WrappedComponent>
+//     )
+// }
+
+const errorHandling = WrappedComponent => {
+    return class extends Component {
+        state = {
+            visible: true
+        }
+
+        handleDismiss = (e) => {
+           console.log("This is the handle dismiss: ", e)
+            this.setState({
+                visible: false
+            })
+        }
+
+        render(){
+            return (
+                 <WrappedComponent>
+                    { this.props.error ? <Message onDismiss={this.handleDismiss} content={this.props.error}/> : null }
+                    { this.props.children }
+                </WrappedComponent>
+            )
+        }
+    }
 }
     
 const DivWithErrorHandling = errorHandling(({ children }) => <div>{children}</div>)
-
 
 export default DivWithErrorHandling
