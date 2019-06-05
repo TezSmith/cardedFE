0279b2cdd0540f1a8be4912ad80c7c1480a481ec
TezSmith <Montez.Smith1@gmail.com>
2019-06-04 20:31:11 -0400

    Fixed styling

--- src/components/ErrorHandle.js
+++ src/components/ErrorHandle.js
@@ -4,46 +4,14 @@ import { Message } from 'semantic-ui-react'
 const errorHandling = WrappedComponent => ({ error, children }) => {
      const [visible, setVisible] = useState(true);
 
-     // const handleDismiss = () => {
-     //     console.log("Yo")
-     // }
-
-     console.log('rendering higher order component')
-
      return (
          <WrappedComponent>
-            {visible ? <Message onDismiss={(e) => console.log('yo', e)} content={error}/> : null}
+            {error && visible ? <Message onDismiss={() => setVisible(false)} content={error}/> : null}
             {children}
         </WrappedComponent>
 
     )
 }
-
-// const errorHandling = WrappedComponent => {
-//     return class extends Component {
-
-//         state = {
-//             visible: true
-//         }
-
-//         handleDismiss = (e) => {
-//           console.log('in handle dismiss: ', e)
-//         //   this.setState({
-//         //         visible: false
-//         //     })
-//         }
-
-//         render(){
-//             console.log("this is the error state: ", this.state)
-//             return (
-//                  <WrappedComponent>
-//                     { this.props.error && this.state.visible ? <Message onDismiss={this.handleDismiss} content={this.props.error}/> : null }
-//                     { this.props.children }
-//                 </WrappedComponent>
-//             )
-//         }
-//     }
-// }
 
 const DivWithErrorHandling = errorHandling(({ children }) => <div>{children}</div>)
 
