d362b4c117c8fe6b5558ad052178a37d35e65f1e
Tez Smith <montez.smith@flatironschool.com>
2019-07-26 12:07:28 -0400

    Fixed persist issue on refresh

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import store from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 const Root = ({ store }) => (
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Router>
       <App/>
     </Router>
+    </PersistGate>
   </Provider>
 )
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import promise from 'redux-promise-middleware'
 // import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/rootReducer';
 
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 // const logger = createLogger()
 const middleware = applyMiddleware(promise, thunk)
 
-const store = createStore(rootReducer, composeWithDevTools(middleware))
+const store = createStore(persistedReducer, composeWithDevTools(middleware))
+const persistor = persistStore(store)
 
-export default store
+
+export { store, persistor }
